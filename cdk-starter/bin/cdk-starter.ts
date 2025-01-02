68235a22f6fc2d162d2b9dabfc64d36d52d71976
Tom <85051403+tomhausmann@users.noreply.github.com>
2025-01-02 12:46:48 +0000

    added spacer-finder folder and initialisied empty ts project

--- cdk-starter/bin/cdk-starter.ts
+++ cdk-starter/bin/cdk-starter.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { CdkStarterStack } from "../lib/cdk-starter-stack";
+import { PhotoStack } from "../lib/PhotoStack";
 
 const app = new cdk.App();
 new CdkStarterStack(app, "CdkStarterStack");
+new PhotoStack(app, "PhotoStack");
---
+++ cdk-starter/lib/PhotoStack.ts
@@ -0,0 +1,22 @@
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import { Bucket, CfnBucket } from "aws-cdk-lib/aws-s3";
+import { Fn } from "aws-cdk-lib";
+
+export class PhotoStack extends cdk.Stack {
+  private stackSuffix: string;
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.initializeSuffix();
+
+    new Bucket(this, "PhotosBucket2", {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
+    });
+  }
+
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split("/", this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split("-", shortStackId));
+  }
+}
